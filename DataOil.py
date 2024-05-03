dfe1c13be39a94cfb59deee5bb3b4e6a938ea6ad
sam.oc <samoconnor6303@gmail.com>
2024-05-02 20:46:54 -0500

    Now the price data is useable within a graph

--- DataOil.py
+++ DataOil.py
@@ -9,20 +9,10 @@ for line in read:
     if line[0] == "\t":
         price.append(line[1:-1])
 
+for i in range(len(price)):
+    price[i] = float(price[i])
+
 print(year)
 print(price)
 
-"""        Jan = month[0]
-        print(month)
-        Feb = month[1]
-        Mar = month[2]
-        Apr = month[3]
-        May = month[4]
-        Jun = month[5]
-        Jul = month[6]
-        Aug = month[7]
-        Sep = month[8]
-        Oct = month[9]
-        Nov = month[10]
-        Dec = month[11]
-"""
+
